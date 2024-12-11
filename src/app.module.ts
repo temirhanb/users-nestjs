6f2e1ee1b521b7ad819d6916c8bc5d42815ab062
temirhanb <temirhan023@gmail.com>
2024-12-11 15:32:48 +0500

    - test deploy nest js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import {Module} from "@nestjs/common";
 import {ConfigModule} from "@nestjs/config";
 import {UsersModule} from "./users/users.module";
+import {ServeStaticModule} from "@nestjs/serve-static";
+import {join} from "path";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "client"),
     }),
     UsersModule,
   ],
